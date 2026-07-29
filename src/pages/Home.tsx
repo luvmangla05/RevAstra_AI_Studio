8962cb7d9c45dca15f86f30a69d474b1ba2a700b
Luv Mangla <luvmangla05@gmail.com>
2026-07-29 17:58:59 +0530

    Login Page Updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
+import Navbar from '../components/Navbar';
 import StrategicChanakyaHero from '../components/StrategicChanakyaHero';
 import '../components/StrategicChanakyaHero.css';
 
 export default function Home() {
-  return <StrategicChanakyaHero />;
+  return (
+    <>
+      <Navbar />
+      <StrategicChanakyaHero />
+    </>
+  );
 }
